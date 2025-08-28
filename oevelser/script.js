9d3cd7ccdcce4fe24f726b5758169ad200da9ddb
buchhavee <mach0015@stud.kea.dk>
2025-08-28 08:38:55 +0200

    hehe

---
+++ oevelser/script.js
@@ -0,0 +1,11 @@
+sayHello("Mads");
+
+function sayHello(firstName) {
+  console.log(`Hello ${firstName}`);
+}
+
+presentPet("Mads", "dog", "Fido");
+
+function presentPet(firstName, animalType, animalName) {
+  console.log(`Hello my name is ${firstName}, i have a ${animalType} called ${animalName}`);
+}
